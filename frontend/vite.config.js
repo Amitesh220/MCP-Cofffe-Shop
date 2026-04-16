ca16fd78610d3929fafb847efd96f729abcd6ea0
Amitesh22 <92164045+Amitesh22@users.noreply.github.com>
2026-04-17 04:47:23 +0530

    First

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,        // allow external connections
     port: 5173,
+    strictPort: true,
+    cors: true,        // allow cross-origin
+    allowedHosts: 'all', // VERY IMPORTANT
     proxy: {
       '/menu': 'http://localhost:3000',
       '/order': 'http://localhost:3000',
